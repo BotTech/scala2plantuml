b60b3f58678b16bf61cd43ff93e50bcaab676071
Jason Pickens <jasonpickensnz@gmail.com>
2021-02-28 19:35:03 +1300

    Fix sort order

--- core/src/main/scala/nz/co/bottech/scala2plantuml/NaturalTypeOrdering.scala
+++ core/src/main/scala/nz/co/bottech/scala2plantuml/NaturalTypeOrdering.scala
@@ -5,50 +5,7 @@ import scala.annotation.tailrec
 
 private[scala2plantuml] object NaturalTypeOrdering extends Ordering[String] {
 
-  private object TypeOrdering extends Ordering[String] {
-
-    override def compare(x: String, y: String): Int = {
-      val xParts = x.splitOnTypes
-      val yParts = y.splitOnTypes
-      if (xParts.length < yParts.length)
-        if (yParts.startsWith(xParts.init)) -1
-        else 0
-      else if (yParts.length < xParts.length)
-        if (xParts.startsWith(yParts.init)) 1
-        else 0
-      else 0
-    }
-  }
-
-  private object PartsOrdering extends Ordering[String] {
-
-    override def compare(x: String, y: String): Int = {
-      val xParts = x.splitOnDigits
-      val yParts = y.splitOnDigits
-      val end    = math.min(xParts.length, yParts.length)
-
-      @tailrec
-      def findFirstDifference(i: Int): Option[Int] =
-        if (i < end)
-          if (xParts(i) != yParts(i)) Some(i)
-          else findFirstDifference(i + 1)
-        else None
-
-      def isDigit(s: String) =
-        Character.isDigit(s.charAt(0))
-
-      def diff(x: String, y: String): Int =
-        if (isDigit(x) && isDigit(y)) math.signum(y.toLong - x.toLong).toInt
-        else x.toLowerCase.compare(y.toLowerCase)
-
-      findFirstDifference(0) match {
-        case Some(i) => diff(xParts(i), yParts(i))
-        case None    => xParts.length - yParts.length
-      }
-    }
-  }
-
-  implicit class StringOps(val str: String) extends AnyVal {
+  implicit private class StringOps(val str: String) extends AnyVal {
 
     def compressWhitespace: String =
       str.strip().replaceAll("""\p{javaWhitespace}+""", " ")
@@ -56,21 +13,43 @@ private[scala2plantuml] object NaturalTypeOrdering extends Ordering[String] {
     def normalize: String =
       Normalizer.normalize(str, Normalizer.Form.NFKC)
 
-    def splitOnTypes: Array[String] =
-      str.split("""(?<=[#.])""", -1)
-
     def splitOnDigits: Array[String] =
       str.split("""(?=\p{Digit})(?<!\p{Digit})|(?<=\p{Digit})(?!\p{Digit})|(?=[#.])|(?<=[#.])""")
   }
 
-  override def compare(x: String, y: String): Int = {
-    val xPrepared = prepare(x)
-    val yPrepared = prepare(y)
-    val typeOrder = TypeOrdering.compare(xPrepared, yPrepared)
-    if (typeOrder == 0) PartsOrdering.compare(xPrepared, yPrepared)
-    else typeOrder
-  }
+  private def prepare(str: String): Array[String] =
+    str.compressWhitespace.normalize.splitOnDigits
 
-  private def prepare(str: String): String =
-    str.compressWhitespace.normalize
+  private def isDigit(s: String) =
+    Character.isDigit(s.charAt(0))
+
+  private def diff(x: String, y: String): Int =
+    if (isDigit(x) && isDigit(y)) math.signum(y.toLong - x.toLong).toInt
+    else x.toLowerCase.compare(y.toLowerCase)
+
+  override def compare(x: String, y: String): Int = {
+    val xParts = prepare(x)
+    val yParts = prepare(y)
+    val end    = math.min(xParts.length, yParts.length)
+
+    @tailrec
+    def findFirstDifference(i: Int): Option[Int] =
+      if (i < end)
+        if (xParts(i) != yParts(i)) Some(i)
+        else findFirstDifference(i + 1)
+      else None
+
+    def isMember(i: Int, a: Array[String]): Boolean =
+      i >= 2 && (i + 1) < a.length && a(i + 1) == "."
+
+    findFirstDifference(0) match {
+      case Some(i) =>
+        if (isMember(i, xParts))
+          if (isMember(i, yParts)) diff(xParts(i), yParts(i))
+          else -1
+        else if (isMember(i, yParts)) 1
+        else diff(xParts(i), yParts(i))
+      case None => xParts.length - yParts.length
+    }
+  }
 }
--- core/src/test/scala/nz/co/bottech/scala2plantuml/NaturalTypeOrderingTests.scala
+++ core/src/test/scala/nz/co/bottech/scala2plantuml/NaturalTypeOrderingTests.scala
@@ -11,23 +11,35 @@ object NaturalTypeOrderingTests extends TestSuite {
     test("numbered after non-numbered") {
       assert(NaturalTypeOrdering.compare("Foo#", "Foo2#") < 0)
     }
-    test("member before nested type") {
-      assert(NaturalTypeOrdering.compare("Foo#a", "Foo#Bar#") < 0)
+    test("member before nested type if earlier") {
+      assert(NaturalTypeOrdering.compare("Foo#a.", "Foo#Bar#") < 0)
     }
-    test("member before nested type even if later") {
-      assert(NaturalTypeOrdering.compare("Foo#c", "Foo#Bar#") < 0)
+    test("member before nested type if same") {
+      assert(NaturalTypeOrdering.compare("Foo#bar.", "Foo#Bar#") < 0)
+    }
+    test("member before nested type if later") {
+      assert(NaturalTypeOrdering.compare("Foo#c.", "Foo#Bar#") < 0)
+    }
+    test("member before double nested type if earlier") {
+      assert(NaturalTypeOrdering.compare("Foo#a.", "Foo#Bar#Baz#") < 0)
+    }
+    test("member before double nested type if same") {
+      assert(NaturalTypeOrdering.compare("Foo#barbaz.", "Foo#Bar#Baz#") < 0)
+    }
+    test("member before double nested type if later") {
+      assert(NaturalTypeOrdering.compare("Foo#c.", "Foo#Bar#Baz#") < 0)
     }
     test("nested before unrelated if earlier") {
-      assert(NaturalTypeOrdering.compare("Foo#Bar#", "Goo#a") < 0)
+      assert(NaturalTypeOrdering.compare("Foo#Bar#", "Goo#a.") < 0)
     }
     test("nested after unrelated if later") {
-      assert(NaturalTypeOrdering.compare("Baz#a", "Foo#Bar#") < 0)
+      assert(NaturalTypeOrdering.compare("Baz#a.", "Foo#Bar#") < 0)
     }
     test("class before object if earlier") {
       assert(NaturalTypeOrdering.compare("Bar#", "Foo.") < 0)
     }
     test("shorter type before longer type") {
-      assert(NaturalTypeOrdering.compare("Foo#bar2", "Foobar#") < 0)
+      assert(NaturalTypeOrdering.compare("Foo#bar2.", "Foobar#") < 0)
     }
   }
 }
